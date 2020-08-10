d8b9e8e352c0907e13546b290d979b1e732d0d35
mikemike1234 <69449385+mikemike1234@users.noreply.github.com>
2020-08-10 16:58:32 +0800

    Add files via upload

---
+++ setblock.py
@@ -0,0 +1,10 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+pos = mc.player.getTilePos()
+
+mc.setBlock(x,y,z,15)
+
+mc.setBlock(x,y,z,15)
+mc.setBlock(x,y,z,15)
+mc.setBlock(x,y,z,15)
